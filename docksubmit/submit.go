694e440a64ea6da016cc8b986c440b2e4bf939ae
Sven Kreiss <sk@svenkreiss.com>
2014-08-12 20:42:05 -0400

    submit mockup.

---
+++ docksubmit/submit.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    "io/ioutil"
+)
+
+func brokerGet(url string) []byte {
+    resp, err := http.Get(url)
+    if err != nil { panic(err) }
+    if resp.StatusCode != 200 {
+        log.Fatal("Unexpected status code", resp.StatusCode)
+    }
+    body, err := ioutil.ReadAll(resp.Body)
+    resp.Body.Close()
+    if err != nil { panic(err) }
+    return body
+}
+
+func main() {
+    log.Printf("Contacting broker on port 4027.\n")
+    log.Printf("Info: %s", brokerGet("http://localhost:4027/info/"))
+    log.Printf("Offer: %s", brokerGet("http://localhost:4027/offer/"))
+}
